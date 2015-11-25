793b6ddc574f8b5711582df68dbfc17c95421451
Yeray Perez <alu0100783612@ull.edu.es>
2015-11-25 13:46:59 +0000

    Travis Full 5

--- spec/MyGema_spec.rb
+++ spec/MyGema_spec.rb
@@ -1,3 +1,4 @@
+#! encoding: UTF-8
 require 'spec_helper'
 require "MyGema/codigo"
 require "MyGema/bibliografia"
@@ -277,7 +278,7 @@ context "Prueba Lista enumerable" do
     end
   end
   
-context 'Bibliografia Enumerable' do
+context 'Bibliografia Comparable' do
     
    before :each do 
        @libro1 = Libro.new(["Dave Thomas, Andy Hunt, Chad Fowler"],"Programming Ruby 1.9 & 2.0: The Pragmatic Programmers","Guide. (The Facets of Ruby).","Pragmatic Bookshelf","4 edition","(July 7, 2013)",["ISBN-13: 978-1937785499,ISBN-10: 1937785491."],1)
